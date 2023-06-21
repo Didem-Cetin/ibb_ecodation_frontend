3d5db3fa47ed0fbf3d87ed68bf9a8cb25a96859f
Didem-Cetin <didimcet97@gmail.com>
2023-06-22 01:07:44 +0300

    .

--- template.js
+++ template.js
@@ -1,1 +1,28 @@
-//alert("Deneme");
+//Dark
+let darkMode=()=>{
+  // alert("DarkMode")
+  window.document.body.classList.toggle("dark_mode")
+}
+
+//input search
+$(document).ready(function(){
+const searchApi=["Adana","Ankara","Balıkesir","Bursa","Ceyhan","Denizli","Diyarbakır","Edirne","Malatya","Zonguldak"];
+$("#tags").autocomplete({
+  source:searchApi
+})
+}); //end 
+  
+
+// Footer
+
+let newDate=()=>{
+  const date=new Date().getFullYear();
+//JS DOM
+//document.getElementById("date_id").innerHTML=new Date().getFullYear()
+
+//Jquery Dom
+
+$("#date_id").html(date);
+}
+
+newDate()
